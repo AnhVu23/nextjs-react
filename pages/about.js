42c22aeadfbd286512b37dbe39697984881b2427
Anh Vu <vu.haianh291@gmail.com>
2021-03-12 15:49:32 +0200

    Feat: Add about page data fetch

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,22 @@
-const About = () => (
-    <div>
-        <h1>About</h1>
-    </div>
-)
+import {Component} from 'react'
+import fetch from 'node-fetch'
+export default class About extends Component {
 
-export default About
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/anhvu23')
+        const data = await res.json()
+        return {
+            user: data
+        }
+    }
+    render() {
+        const {user} = this.props
+        if (!user) return <div>Loading...</div>
+        return (
+            <div>
+                <h1>About</h1>
+                <img src={user.avatar_url}/>
+            </div>
+        )
+    }
+}
