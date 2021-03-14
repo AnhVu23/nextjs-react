6b582ff6f929ce0a6da6d00dc41dfc2ba82cc1f0
Anh Vu <vu.haianh291@gmail.com>
2021-03-14 16:26:56 +0200

    Feat: Add hackernew endpoints

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,33 @@
-import Link from 'next/link'
-const Home = ({
+import fetch from 'node-fetch'
+import React from 'react'
+import Error from 'next/error'
 
-}) => (
-    <div>
-        <h1>Home</h1>
-        <Link href='/about'>
-            <a>Go to about</a>
-        </Link>
-        <p>Welcome to the home page!</p>
-    </div>
-)
+export default class Home extends React.Component {
+    static async getInitialProps() {
+        let stories
+        try {
+            const res = await fetch('https://node-hnapi.herokuapp.com/news')
+            stories = await res.json()
 
-export default Home
+        } catch (e) {
+            console.log(e)
+            stories = []
+        }
+        return {
+            stories
+        }
+    }
+
+    render() {
+        const {stories} = this.props
+        if (stories.length === 0) return <Error statusCode={404}/>
+        return (
+            <div>
+                <h1>hacker next</h1>
+                {stories.map(story => (
+                    <h2 key={story.id}>{story.title}</h2>
+                ))}
+            </div>
+        )
+    }
+}
